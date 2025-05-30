9f37b3733741c115e5c111bfb47aca9ade9b20da
Abel George Stanley <abel@kalayil.com>
2025-05-31 00:08:08 +0530

    Update StringRotation.java

--- CTCI/StringRotation.java
+++ CTCI/StringRotation.java
@@ -1,8 +1,8 @@
 public class StringRotation {
     public static void main(String[] args) {
-        StringBuilder s1=new StringBuilder("waterbottle");
-        StringBuilder s2=new StringBuilder("erbottlewat");
-        System.out.println(strRot(s1,s2));
+        StringBuilder s1=new StringBuilder("abcdeabc");
+        StringBuilder s2=new StringBuilder("deabcabc");
+        System.out.println(isStrRot(s1,s2));
 
     }
 
@@ -10,26 +10,32 @@ public class StringRotation {
         return str.toString().contains(sub.toString());
     }
 
-    public static boolean strRot(StringBuilder s1,StringBuilder s2){
-        if (s1.length()!=s2.length()) {
-            return false;
-        }
-        StringBuilder str=new StringBuilder();
-        int j=0;
-        for(int i=0;i<s2.length();i++){
-            if (s1.charAt(j)==s2.charAt(i)) {
-                str.append(s2.charAt(i));
-                j++;
-            }else{
-                j=0;
-                str=new StringBuilder();
-            }
-        }
+    // public static boolean strRot(StringBuilder s1,StringBuilder s2){
+    //     if (s1.length()!=s2.length()) {
+    //         return false;
+    //     }
+    //     StringBuilder str=new StringBuilder();
+    //     int j=0;
+    //     for(int i=0;i<s2.length();i++){
+    //         if (s1.charAt(j)==s2.charAt(i)) {
+    //             str.append(s2.charAt(i));
+    //             j++;
+    //         }else{
+    //             j=0;
+    //             i--;
+    //             str=new StringBuilder();
+    //         }
+    //     }
 
-        int l=str.length();
-        int sl=s1.length();
-        StringBuilder x=new StringBuilder(s2.substring(0,sl-l));
-        return isSubstring(s1, x);
+    //     int l=str.length();
+    //     int sl=s1.length();
+    //     StringBuilder x=new StringBuilder(s2.substring(0,sl-l));
+    //     return isSubstring(s1, x);
+    // }
+
+    public static boolean isStrRot(StringBuilder s1,StringBuilder s2){
+        StringBuilder con=s1.append(s1);
+        return isSubstring(con, s2);
     }
     
 }
